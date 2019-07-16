42846528f37c0b0ce81002e6560c051f3a2297ca
yxrb <yxrb95198@163.com>
2019-07-16 16:53:59 +0800

    2

--- boostrap4.x/index.js
+++ boostrap4.x/index.js
@@ -19,6 +19,7 @@ function mouseclick(){
 
 let bImg = document.querySelector('.banner img')
 let lbt = document.querySelector('.lbt')
+let ban = document.querySelectorAll('.banner')
 console.log(document.querySelector('.banner img').clientHeight)
 let h = document.querySelector('.banner img').clientHeight
 lbt.style.height = h + "px"
@@ -26,15 +27,20 @@ lbt.style.height = h + "px"
 let dotli = document.querySelectorAll('.li')
 console.log(dotli)
 for(let i=0;i<dotli.length;i++){
-    dotli[i].addEventListener('mouseover',dotclick)
+    dotli[i].addEventListener('click',dotclick)
 }
 
 function dotclick(){
-    console.log(2)
+    // console.log(2)
+    let list = Array.from(dotli)
     // console.log(this.style.className)
     for(let y=0;y<dotli.length;y++){
         dotli[y].className = "li"
+        ban[y].className = "banner"
     }
+    let index = list.indexOf(this)
+    ban[index].className = "banner b-active"
     this.className = "dot-active li"
-    console.log(this.className)
+    
+    console.log(index)
 }
